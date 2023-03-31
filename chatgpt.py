66469b1580ea0cad51f8e11942e5197362713f1f
Manash Baul <manash.baul@inmobi.com>
2023-04-01 02:02:18 +0530

    feat(response): Added Markdown Display

--- chatgpt.py
+++ chatgpt.py
@@ -1,16 +1,16 @@
 #!/bin/env python
 
 import atexit
-import click
 import os
-import requests
 import sys
-import yaml
 
+import click
+import requests
+import yaml
 from prompt_toolkit import PromptSession, HTML
 from prompt_toolkit.history import FileHistory
 from rich.console import Console
-from rich.markup import escape
+from rich.markdown import Markdown
 
 CONFIG_FILE = "config.yaml"
 BASE_ENDPOINT = "https://api.openai.com/v1"
@@ -21,7 +21,6 @@ PRICING_RATE = {
     "gpt-4": {"prompt": 0.03, "completion": 0.06},
     "gpt-4-32k": {"prompt": 0.06, "completion": 0.12},
 }
-
 
 # Initialize the messages history list
 # It's mandatory to pass it at each API call in order to have a conversation
@@ -35,7 +34,7 @@ console = Console()
 
 def load_config(config_file: str) -> dict:
     """
-    Read a YAML config file and returns it's content as a dictionary
+    Read a YAML config file and returns its content as a dictionary
     """
     with open(config_file) as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
@@ -44,16 +43,16 @@ def load_config(config_file: str) -> dict:
 
 
 def calculate_expense(
-    prompt_tokens: int,
-    completion_tokens: int,
-    prompt_pricing: float,
-    completion_pricing: float,
+        prompt_tokens: int,
+        completion_tokens: int,
+        prompt_pricing: float,
+        completion_pricing: float,
 ) -> float:
     """
     Calculate the expense, given the number of tokens and the pricing rates
     """
     expense = ((prompt_tokens / 1000) * prompt_pricing) + (
-        (completion_tokens / 1000) * completion_pricing
+            (completion_tokens / 1000) * completion_pricing
     )
     return round(expense, 6)
 
@@ -75,72 +74,135 @@ def display_expense(model) -> None:
 def start_prompt(session, config):
     # TODO: Refactor to avoid a global variables
     global prompt_tokens, completion_tokens
+    message = session.prompt(HTML(f"<b>[{prompt_tokens + completion_tokens}] >>> </b>"))
+    check_exit(message)
+    add_user_message(message)
+    response = send_request(config)
+    handle_response(response)
 
-    headers = {
+
+def get_body(model):
+    return {"model": model, "messages": messages}
+
+
+def get_headers(api_key):
+    return {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {config['api-key']}",
+        "Authorization": f"Bearer {api_key}",
     }
 
-    message = session.prompt(HTML(f"<b>[{prompt_tokens + completion_tokens}] >>> </b>"))
 
-    if message.lower() == "/q":
+def send_request(conf):
+    try:
+        body = get_body(conf["model"])
+        headers = get_headers(conf["api-key"])
+        return requests.post(f"{BASE_ENDPOINT}/chat/completions", headers=headers, json=body)
+    except requests.ConnectionError as ex:
+        raise_connection_error(ex)
+    except requests.Timeout as ex:
+        raise_connection_error(ex)
+
+
+def raise_connection_error(ex):
+    console.print("Connection Error: {}".format(ex), style="red bold")
+    messages.pop()
+    raise KeyboardInterrupt
+
+
+def check_exit(message):
+    if message.lower() == "/q" or message.lower() == "exit":
         raise EOFError
+    if message.lower() == "/c" or message.lower() == "clear":
+        messages.clear()
+        add_default_context()
     if message.lower() == "":
         raise KeyboardInterrupt
 
-    messages.append({"role": "user", "content": message})
 
-    body = {"model": config["model"], "messages": messages}
-
-    try:
-        r = requests.post(
-            f"{BASE_ENDPOINT}/chat/completions", headers=headers, json=body
-        )
-    except requests.ConnectionError:
-        console.print("Connection error, try again...", style="red bold")
-        messages.pop()
-        raise KeyboardInterrupt
-    except requests.Timeout:
-        console.print("Connection timed out, try again...", style="red bold")
-        messages.pop()
-        raise KeyboardInterrupt
-
+def handle_response(r):
+    global prompt_tokens, completion_tokens
     if r.status_code == 200:
-        response = r.json()
-
-        message_response = response["choices"][0]["message"]
-        usage_response = response["usage"]
-
-        console.print(escape(message_response["content"].strip()))
-
-        # Update message history and token counters
-        messages.append(message_response)
-        prompt_tokens += usage_response["prompt_tokens"]
-        completion_tokens += usage_response["completion_tokens"]
+        handle_success_response(r)
 
     elif r.status_code == 400:
-        response = r.json()
-        if "error" in response:
-            if response["error"]["code"] == "context_length_exceeded":
-                console.print("Maximum context length exceeded", style="red bold")
-                raise EOFError
-                # TODO: Develop a better strategy to manage this case
-        console.print("Invalid request", style="bold red")
-        raise EOFError
+        bad_request_error(r)
 
     elif r.status_code == 401:
-        console.print("Invalid API Key", style="bold red")
-        raise EOFError
+        invalid_api_key_error()
 
     elif r.status_code == 429:
-        console.print("Rate limit or maximum monthly limit exceeded", style="bold red")
-        messages.pop()
-        raise KeyboardInterrupt
+        rate_limit_error()
 
     else:
         console.print(f"Unknown error, status code {r.status_code}", style="bold red")
         console.print(r.json())
         raise EOFError
+
+
+def bad_request_error(r):
+    """
+        Handle a bad request error like context length exceeded or invalid request.
+    """
+    response = r.json()
+    if "error" in response:
+        if response["error"]["code"] == "context_length_exceeded":
+            console.print("Maximum context length exceeded", style="red bold")
+            raise EOFError
+            # TODO: Develop a better strategy to manage this case
+    console.print("Invalid request", style="bold red")
+    raise EOFError
+
+
+def invalid_api_key_error():
+    """
+        Handle an invalid API Key
+    """
+    console.print("Invalid API Key", style="bold red")
+    raise EOFError
+
+
+def rate_limit_error():
+    """
+        Handle a rate limit error
+    """
+    console.print("Rate limit or maximum monthly limit exceeded", style="bold red")
+    messages.pop()
+    raise KeyboardInterrupt
+
+
+def handle_success_response(r):
+    """
+        Handle a successful response from the API
+    """
+    global prompt_tokens, completion_tokens
+    response = r.json()
+    message_response = response["choices"][0]["message"]
+    usage_response = response["usage"]
+    console.print(Markdown((message_response["content"].strip())))
+    console.rule()
+    # Update message history and token counters
+    messages.append(message_response)
+    prompt_tokens += usage_response["prompt_tokens"]
+    completion_tokens += usage_response["completion_tokens"]
+
+
+def add_default_context():
+    """
+        Default Context to Show Markdown Formatting
+    """
+    messages.append(
+        {
+            "role": "system",
+            "content": "Give all the responses in markdown format. Always use code blocks to when writing code."
+        }
+    )
+
+
+def add_user_message(message):
+    """
+        Add User Message to Context
+    """
+    messages.append({"role": "user", "content": message})
 
 
 @click.command()
@@ -170,10 +232,10 @@ def main(context, api_key) -> None:
 
     # Run the display expense function when exiting the script
     atexit.register(display_expense, model=config["model"])
+    console.rule("[bold]ChatGPT CLI[/bold]", style="bold")
+    console.rule(f"Model in use: [green bold]{config['model']}")
 
-    console.print("ChatGPT CLI", style="bold")
-    console.print(f"Model in use: [green bold]{config['model']}")
-
+    add_default_context()
     # Context from the command line option
     if context:
         console.print(f"Context file: [green bold]{context.name}")
